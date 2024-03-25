5a941972fe3da230764a944e2bd45feabdc1e1be
Dmitri Gustafsson <>
2024-03-25 10:39:51 +0100

    Initial commit

---
+++ src/test/java/com/exam/surveytool/SurveyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.surveytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
