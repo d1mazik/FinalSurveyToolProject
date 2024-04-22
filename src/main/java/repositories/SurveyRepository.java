73198e0a5340c821641413e7407b9c4a8c4144b4
JesperSchon <jesper.schon@yh.nackademin.se>
2024-04-22 11:41:45 +0200

    Created SurveyRepository
    Extends JpaRepository

---
+++ src/main/java/repositories/SurveyRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import models.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+}
+
