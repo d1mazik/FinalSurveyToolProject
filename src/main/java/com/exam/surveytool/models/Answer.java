f5c099f43feb50b4e7ade33010a6fdb1c2c49cc9
Dmitri Gustafsson <>
2024-04-23 13:39:49 +0200

    fixed User, Role, Answer and SurveyResponseSession models

--- src/main/java/com/exam/surveytool/models/Answer.java
+++ src/main/java/com/exam/surveytool/models/Answer.java
@@ -1,4 +1,46 @@
 package com.exam.surveytool.models;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "answer")
+@Data
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_response_session_id")
+    private SurveyResponseSession surveyResponseSession;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/exam/surveytool/models/Role.java
+++ src/main/java/com/exam/surveytool/models/Role.java
@@ -1,4 +1,24 @@
 package com.exam.surveytool.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
 }
