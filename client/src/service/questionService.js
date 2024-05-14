0037034c0f488d47403ac7a03a71d2aac40072a3
Dmitri Gustafsson <>
2024-05-14 18:19:27 +0200

    fixat sida där man addar frågor och surveys

--- client/src/service/questionService.js
+++ client/src/service/questionService.js
@@ -1,5 +1,3 @@
-// questionService.js
-
 export const getSurveyTitle = async (surveyId) => {
     try {
         const response = await fetch(`/api/surveys/${surveyId}`);
@@ -10,6 +8,64 @@ export const getSurveyTitle = async (surveyId) => {
         return data.title; // Returnerar titeln på undersökningen från API-responsen
     } catch (error) {
         console.error('Error fetching survey title:', error);
+        throw error;
+    }
+};
+
+export const getSurveys = async () => {
+    try {
+        const response = await fetch('/api/surveys');
+        if (!response.ok) throw new Error('Network response was not ok');
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching surveys:', error);
+        throw error;
+    }
+};
+
+export const createSurvey = async (surveyData) => {
+    try {
+        const response = await fetch('/api/surveys', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(surveyData)
+        });
+        if (!response.ok) throw new Error('Failed to create survey');
+        return await response.json();
+    } catch (error) {
+        console.error('Error creating survey:', error);
+        throw error;
+    }
+};
+
+export const updateSurvey = async (id, surveyData) => {
+    try {
+        const response = await fetch(`/api/surveys/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(surveyData)
+        });
+        if (!response.ok) throw new Error('Failed to update survey');
+        return await response.json();
+    } catch (error) {
+        console.error('Error updating survey:', error);
+        throw error;
+    }
+};
+
+export const deleteSurvey = async (id) => {
+    try {
+        const response = await fetch(`/api/surveys/${id}`, {
+            method: 'DELETE'
+        });
+        if (!response.ok) throw new Error('Failed to delete survey');
+        return 'Survey deleted successfully';
+    } catch (error) {
+        console.error('Error deleting survey:', error);
         throw error;
     }
 };
@@ -81,5 +137,4 @@ export const deleteQuestion = async (id) => {
         console.error('Error deleting question:', error);
         throw error;
     }
-
 };
